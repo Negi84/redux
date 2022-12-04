c539705cb3d81f9e4b1f4454191b9d99b5c85dbc
Vipin Negi <vipin@mindrops.com>
2022-12-04 09:49:12 +0530

    Working with Multiple State Properties

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -8,6 +8,7 @@ import classes from "./Counter.module.css";
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
   //we are extracting the part of the state, counter is the state which we are extracting here
   //React redux automatically setup the subscription for this component to react store
 
@@ -18,17 +19,21 @@ const Counter = () => {
   const increaseHandler = () => {
     dispatch({ type: "increase", amount: 5 });
   };
-  
+
   const decrementHandler = () => {
     dispatch({ type: "decrement" });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({
+      type: "toggle",
+    });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Increase By 5</button>
--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,38 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
+    console.log(state);
     return {
       counter: state.counter + 1,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === "decrement") {
+    console.log(state);
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === "increase") {
+    console.log(state);
+
     return {
       counter: state.counter + action.amount,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "toggle") {
+    console.log(state);
+
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
     };
   }
 
